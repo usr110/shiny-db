eb343860edd390f08e9e5f71376f3bfdc3a275c3
usr110 <m.aliabbas@gmail.com>
2017-03-02 13:27:28 +0000

    Load coounty data when map bounds change

--- shiny_app.R
+++ shiny_app.R
@@ -1,11 +1,29 @@
 library(leaflet)
+library(geojsonio)
 library(DBI)
+library(RMySQL)
+
 
 conn <- NULL
+dbdata <- NULL
 
 db_cred <- read.csv("db-info.csv", header = T, stringsAsFactors = F)
 
 server <- function(input, output) {
+  
+  # Initialize the leaflet map
+  output$map <- renderLeaflet(
+    leaflet() %>%
+      addTiles(., 
+               attribution = '<a target="_blank" href="http://shiny.rstudio.com/">Shiny</a> |
+               Map &copy <a target="_blank" href ="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+               options = tileOptions(minZoom = 7,
+                                     maxZoom = 18, 
+                                     reuseTiles = T)) %>%
+      setView(lng = -1.5, lat = 53.4, zoom = 10) %>%
+      mapOptions(zoomToLimits = "first") #%>%
+      )
+  
   
   observe({
       all_cons <- dbListConnections(MySQL())
@@ -16,54 +34,26 @@ server <- function(input, output) {
       print(paste(length(all_cons), " connections killed."))
   })
   
-  
-  
-  # Initialize the leaflet map
-  output$map <- renderLeaflet(
-    leaflet() %>%
-      addTiles(., 
-               attribution = '<a target="_blank" href="http://shiny.rstudio.com/">Shiny</a> |
-               Map &copy <a target="_blank" href ="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors') %>%
-      setView(lng = -1.5, lat = 53.4, zoom = 10) %>%
-      #addMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R") %>%
-      mapOptions(zoomToLimits = "first")
-  )
-  
-  # Returns the map bounding box
-  map_bb <- reactive({
+  observe({
+    
     if (is.null(input$map_bounds)){ return (NULL)}
+    
     x1 <- input$map_bounds$east
     x0 <- input$map_bounds$west
     y0 <- input$map_bounds$south
     y1 <- input$map_bounds$north
-    #(x0, y0), (x0, y1), (x1, y1), (x1, y0), (x0, y0)
-    #cat(" ----------- \n")
-    #res <- paste( x0, y0, ", ",  "\n" , x0, y1, ", " ,  "\n", x1, y1, ", " ,  "\n",  x1, y0, ", " , "\n", x0, y0 )
-    #cat(res, "\n")
-    #cat(" ----------- \n")
     
-    return(c(x0, x1, y0, y1))
-  })
   
-  observe({
+    qry <- paste0("SELECT `name`, `geometry`, AsText(`bbox`) as bbox
+                      FROM `county`
+                      WHERE MBRIntersects(`bbox`, ST_GeomFromText('Polygon((
+                      ", x0, " ", y0, ", ",
+                  x0, " ", y1, ", ",
+                  x1, " ", y1, ", ",
+                  x1, " ", y0, ", ",
+                  x0, " ", y0, " ))'));")
     
-    bbox <- map_bb()
-    
-    cat("length : ", length(bbox), "\n")
-    if (is.null(bbox) || length(bbox) != 4)
-      return()
-    
-    
-    x0 <- bbox[1]
-    x1 <- bbox[2]
-    y0 <- bbox[3]
-    y1 <- bbox[4]
-    
-    #(x0, y0), (x0, y1), (x1, y1), (x1, y0), (x0, y0)
-    #cat(" ----------- \n")
-    
-    #cat(res, "\n")
-    #cat(" ----------- \n")
+    # cat(qry, "\n")
     
     conn <- dbConnect(
       drv = RMySQL::MySQL(),
@@ -73,102 +63,144 @@ server <- function(input, output) {
       password = db_cred$pwd
     )
     
-    qry <- paste0("SELECT `lsoa`.`code`, `lsoa`.`name`, `lsoa`.`geometry`, AsText(`lsoa`.`bbox`) as bbox
-                    FROM `lsoa`
-                    WHERE MBRIntersects(`lsoa`.`bbox`, ST_GeomFromText('Polygon((
-                    ", x0, " ", y0, ", ",
-                    x0, " ", y1, ", ",
-                    x1, " ", y1, ", ",
-                    x1, " ", y0, ", ",
-                    x0, " ", y0, " ))'));")
-    
-    
-    cat(qry, "\n")
-    
     rs <- dbSendQuery(conn, qry)
     
-    # rs <- dbSendQuery(conn, paste0("
-    #                   SELECT `lsoa`.`code`, `lsoa`.`name`, `lsoa`.`geometry`, AsText(`lsoa`.`bbox`) as bbox
-    #                   FROM `lsoa`
-    #                   WHERE MBRIntersects(`lsoa`.`bbox`, ST_GeomFromText('Polygon(
-    #                   ", x0, " ", y0, ", ",
-    #                      x0, " ", y1, ", ",
-    #                      x1, " ", y1, ", ",
-    #                      x1, " ", y0, ", ",
-    #                      x0, " ", y0, " )));"))
-    
-                      
-    # cat(bbox[1], bbox[2], bbox[3], bbox[4], "\n")
-    
     dbdata <- dbFetch(rs)
-    cat("Nrows: ", nrow(dbdata), "\n")
+
     dbDisconnect(conn)
-    
+
     if (!is.null(dbdata) && nrow(dbdata) > 0){
       
-      cat("Nrows: ", nrow(dbdata), "\n")
+      clearGroup(leafletProxy("map"), c("poly"))
       
-      dat <- get_geojson_data(dbdata)
+      local_dat <- get_geojson_data(dbdata)
+
+      leafletProxy("map")  %>% addPolygons(data = local_dat,
+                                           popup = local_dat$name,
+                                           group = "poly",
+                                           color = "black",
+                                           opacity = 0.7,
+                                           layerId = paste0(local_dat$name, '-', "zones"))
       
-      leafletProxy("map")  %>% removeShape(., "highlighted")
-      
-       isolate({
-          addPolygons(leafletProxy("map"), 
-                      data = dat,
-                      color = "black",
-                      opacity = 0.7 ,
-                      layerId = "highlighted")
-     })
-    
     }
-      
   })
+  
+  # Returns the map bounding box
+  map_bb <- reactive({
+    #input$map_zoom
+    
+    if (is.null(input$map_bounds)){ return (NULL)}
+    # x1 <- isolate(input$map_bounds$east)
+    # x0 <- isolate(input$map_bounds$west)
+    # y0 <- isolate(input$map_bounds$south)
+    # y1 <- isolate(input$map_bounds$north)
+    
+    x1 <- input$map_bounds$east
+    x0 <- input$map_bounds$west
+    y0 <- input$map_bounds$south
+    y1 <- input$map_bounds$north
+    
+    return(c(x0, x1, y0, y1))
+  })
+  
+  # observe({
+  #   
+  #   #input$map_zoom
+  #   
+  #   bbox <- map_bb()
+  # 
+  #   cat("length : ", length(bbox), "\n")
+  #   if (is.null(bbox) || length(bbox) != 4)
+  #     return()
+  # 
+  #   x0 <- bbox[1]
+  #   x1 <- bbox[2]
+  #   y0 <- bbox[3]
+  #   y1 <- bbox[4]
+  # 
+  #   conn <- dbConnect(
+  #     drv = RMySQL::MySQL(),
+  #     dbname = db_cred$schema,
+  #     host = db_cred$host,
+  #     username = db_cred$username,
+  #     password = db_cred$pwd
+  #   )
+  # 
+  #   qry <- paste0("SELECT `name`, `geometry`, AsText(`bbox`) as bbox
+  #                   FROM `county`
+  #                   WHERE MBRIntersects(`bbox`, ST_GeomFromText('Polygon((
+  #                   ", x0, " ", y0, ", ",
+  #                   x0, " ", y1, ", ",
+  #                   x1, " ", y1, ", ",
+  #                   x1, " ", y0, ", ",
+  #                   x0, " ", y0, " ))'));")
+  # 
+  # 
+  #   #cat(qry, "\n")
+  # 
+  #   rs <- dbSendQuery(conn, qry)
+  # 
+  #   dbdata <- dbFetch(rs)
+  # 
+  #   #cat("Nrows: ", nrow(dbdata), "\n")
+  # 
+  #   dbDisconnect(conn)
+  # 
+  #   if (!is.null(dbdata) && nrow(dbdata) > 0){
+  #     get_geojson_data(dbdata)
+  #     
+  #     #geojson_write(dat)
+  #     dat <- geojson_read("test.geojson", what = "sp")
+  #     cat("Nrows: ", nrow(dat@data), "\n")
+  #     # %>% removeShape("polygons")
+  #     leafletProxy("map")  %>% addPolygons(data = dat, # geojson_read("myfile.geojson", what = "sp"),
+  #                                          popup = dat$name,
+  #                                          color = "black",
+  #                                          opacity = 0.7 ,
+  #                                          layerId = "polygons")
+  #   }
+  #   
+  # })
       
-      get_geojson_data <- function(dbdata){
+  get_geojson_data <- function(raw_data){
+    
+    test = ""
+    for (i in 1:nrow(raw_data)){
       
-      test = ""
-      for (i in 1:nrow(dbdata)){
-        
-        if (i == 1){
-          
-          test = '
+      if (i == 1){
+        test = '
           {
           "type": "FeatureCollection",
           "features": [  '
-          }
-        
-        test = paste0(test, '
-                      
-                      {
-                      "type": "Feature",
-                      "id": ',  '"' ,  dbdata$name[i],'"' , ',
-                      "geometry": {
-                      "type": "Polygon",
-                      "coordinates": ',  
-                      dbdata$geometry[i], '
-                      }
-                      },')
-  
-        # remove the last comma
-        if (i == nrow(dbdata)){
-          test = substr(test, 1, nchar(test) - 1)
-          test = paste0(test, ' ]}') 
-        }
-        
-                      }
-      
-      test <- as.json(test)
-      
-      return(geojson_sp(test))
       }
       
+      test = paste0(test, '{
+                      "type": "Feature",
+                      "name": ',  '"' ,  raw_data$name[i],'"' , ',
+                      "geometry": {
+                      "type": "Polygon",
+                      "coordinates": ', raw_data$geometry[i], '
+                       }
+                      },')
       
+      # remove the last comma
+      if (i == nrow(raw_data)){
+        test = substr(test, 1, nchar(test) - 1)
+        test = paste0(test, ' ]}')
+      }
       
+    }
+    sink(tempfile())
+    test <- as.json(test)
+    sink()
+    return(geojson_sp(test))
+  }
+  
 }
 
 ui <- fluidPage(
   
-  leafletOutput("map", width="100%", height= 400)
+  leafletOutput("map", width="100%", height= 800)
 )
 
 shinyApp(ui = ui, server = server)
